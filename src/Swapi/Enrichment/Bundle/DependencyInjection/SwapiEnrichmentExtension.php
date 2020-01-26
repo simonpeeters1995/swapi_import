a3c6813607db9e6b3302e459f74690f46d74d4e8
simon peeters <simon@simons-MacBook-Pro.local>
2020-01-26 15:35:11 +0100

    reset to standard

---
+++ src/Swapi/Enrichment/Bundle/DependencyInjection/SwapiEnrichmentExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace swapi\Enrichment\Bundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+
+class SwapiEnrichmentExtension extends Extension
+{
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('jobs.yml');
+        $loader->load('steps.yml');
+
+    }
+}
---
+++ src/Swapi/Enrichment/Bundle/SwapiEnrichmentBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace swapi\Enrichment\Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class SwapiEnrichmentBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+
+    }
+}
